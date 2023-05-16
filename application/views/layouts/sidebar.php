5807288a109b152f84a0f30c863c2b8690ce7e0b
=farahhisanah <=frah3393@gmail.com>
2023-05-16 13:45:01 +0700

    sidebar change

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -3,10 +3,10 @@
 
 <!-- Sidebar - Brand -->
 <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-	<div class="sidebar-brand-icon rotate-n-15">
-		<i class="fas fa-laugh-wink"></i>
+	<div class="sidebar-brand-icon">
+	<img src="<?= base_url('assets/img/logo.png'); ?>" alt="" style="height: 30px;">
 	</div>
-	<div class="sidebar-brand-text mx-3">BNI Corporate</div>
+	<div class="sidebar-brand-text mx-3">BNI Corporate </div>
 </a>
 
 <!-- Divider -->
@@ -20,7 +20,7 @@
 <!-- Nav Item - Dashboard -->
 <li class="nav-item">
 	<a class="nav-link" href="index.html">
-		<i class="fas fa-fw fa-tachometer-alt"></i>
+	<i class="fas fa-thin fa-newspaper"></i>
 		<span>Dashboard</span></a>
 </li>
 
