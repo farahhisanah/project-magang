52c18f84980ab1b62c94d87f29173875180f1675
=farahhisanah <=frah3393@gmail.com>
2023-05-17 16:00:08 +0700

    neww

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -3,7 +3,7 @@
  <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 	<!-- Sidebar - Brand -->
-	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
 		<div class="sidebar-brand-icon">
 		<img src="<?= base_url('assets/img/logo.png'); ?>" alt="" style="height: 30px;">
 		</div>
--- application/views/user/index.php
+++ application/views/user/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-	<?php $this->load->view("layouts/header") ?>
+	<?php $this->load->view('layouts/header') ?>
 </head>
 
 <body id="page-top">
@@ -10,7 +10,7 @@
 	<!-- Page Wrapper -->
 	<div id="wrapper">
 
-	<?php $this->load->view("layouts/sidebar") ?>
+	<?php $this->load->view('layouts/sidebar') ?>
 
 	<!-- Heading -->
 		
@@ -20,21 +20,12 @@
             <!-- Main Content -->
             <div id="content">
 
-			<?php $this->load->view("layouts/topbar") ?>
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">BNI SMARTER</h1>
-
-                </div>
-                <!-- /.container-fluid -->
+			<?php $this->load->view('layouts/topbar') ?>
 
             </div>
             <!-- End of Main Content -->
 		
-		<?php $this->load->view("layouts/footer") ?>
+		<?php $this->load->view('layouts/footer') ?>
 
 </body>
 </html>
