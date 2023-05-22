72e5e712175b2a68a630270a4f4c40af5d49c609
nazzzlah <nazlahatthahirahdwimelki@gmail.com>
2023-05-22 16:13:08 +0700

    report kontroller

---
+++ application/controllers/Report.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ *	@property db $db
+ *	@property session $session
+*/
+
+class Report extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function index()
+	{
+		$data['user'] = $this->db->get_where('user', ['email' =>
+		$this->session->userdata('email')])->row_array();
+		$this->load->view('report/index', $data);
+		
+	}
+}
