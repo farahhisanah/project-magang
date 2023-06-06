b2da297d2bfe06576224667e6a9dc3546be6ac90
nazzzlah <nazlahatthahirahdwimelki@gmail.com>
2023-06-06 16:58:30 +0700

    testting

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -4,15 +4,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  *	@property db $db
  *	@property session $session
+ *  @property Report_model $Report_model
+ *	@property form_validation
 */
 
 class Report extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('Report_model');
+
+	}
 
 	public function index()
 	{
 		$data['title'] = 'Report';
 		$data['user'] = $this->db->get_where('user', ['email' =>
 		$this->session->userdata('email')])->row_array();
+
+		$data["report"] = $this->Report_model->getAll();
 
 		$this->load->view('layouts/header', $data);
 		$this->load->view('layouts/sidebar', $data);
@@ -21,4 +32,217 @@ class Report extends CI_Controller {
 		$this->load->view('layouts/footer', $data);
 		
 	}
+
+	public function submit_insert()
+	{
+		// print_r('<pre>');
+		// print_r($this->input->post());
+		// print_r($this->input->file());
+		// die;
+
+		
+		$capture_name = "";
+		if(isset($_FILES['capture'])) {
+			
+			$config['upload_path'] = './uploads/'; 
+			$config['allowed_types'] = 'jpg|jpeg|png|pdf|'; 
+			$config['max_size'] = '10000'; // max_size in kb 
+			$config['encrypt_name'] = TRUE;
+
+			$this->load->library('upload', $config);
+
+			if ($this->upload->do_upload('capture')) {
+				$capture = $this->upload->data();
+				$capture_name = $capture['file_name'];
+			}
+			
+		}
+
+		$query_name = "";
+		if(isset($_FILES['query'])) {
+			
+			$config['upload_path'] = './uploads/'; 
+			$config['allowed_types'] = 'pdf|sql|doc|docx|xlsx'; 
+			$config['max_size'] = '10000'; // max_size in kb 
+			$config['encrypt_name'] = TRUE;
+
+			$this->load->library('upload', $config);
+
+			if ($this->upload->do_upload('query')) {
+				$query = $this->upload->data();
+				$query_name = $query['file_name'];
+			}
+			
+		}
+
+		$data['title'] = 'Form Insert Report';
+		$data['user'] = $this->db->get_where('user', ['email' =>
+						$this->session->userdata('email')])->row_array();
+
+		$this->form_validation->set_rules('nama_report', 'Nama Report', 'required');
+
+		if( $this->form_validation->run() == FALSE ){
+			$this->load->view('layouts/header', $data);
+			$this->load->view('layouts/sidebar', $data);
+			$this->load->view('layouts/topbar', $data);
+			$this->load->view('report/insert', $data);
+			$this->load->view('layouts/footer', $data);
+		}else{
+
+			$data = [
+			"nama_report"=>$this->input->post('nama_report', true),
+			"npp"=>$this->input->post('npp', true),
+			"nama"=>$this->input->post('nama', true),
+			"unit"=>$this->input->post('unit', true),
+			"via"=>$this->input->post('via', true),
+			"nomor_email"=>$this->input->post('nomor_email', true),
+			"tgl_permintaan"=>$this->input->post('tgl_permintaan', true),
+			"capture"=>$capture_name,
+			"pic"=>$this->input->post('pic', true),
+			"query"=>$query_name,
+			"tgl_diberikan"=>$this->input->post('tgl_diberikan', true),
+			"status"=>$this->input->post('status', true)
+			];
+
+			$this->Report_model->insertReport($data);
+			$this->session->set_flashdata('flash', '<div class="alert alert-success" role="alert"> Insert Success</div>');
+			redirect('report');
+		}
+
+		/*if($this->db->affected_rows() > 0) {
+			$this->session->set_flashdata('flash', '<div class="alert alert-success" role="alert"> Insert Success</div>');
+		} else {
+			$this->session->set_flashdata('flash', '<div class="alert alert-danger" role="alert"> Insert Failed</div>');
+		}
+		redirect('report');*/
+
+	}
+
+	public function insert()
+	{
+		$data['title'] = 'Form Insert Report';
+		$data['user'] = $this->db->get_where('user', ['email' =>
+						$this->session->userdata('email')])->row_array();
+
+			$this->load->view('layouts/header', $data);
+			$this->load->view('layouts/sidebar', $data);
+			$this->load->view('layouts/topbar', $data);
+			$this->load->view('report/insert', $data);
+			$this->load->view('layouts/footer', $data);
+		
+	}
+
+	public function delete($id)
+	{
+		$this->Report_model->deleteReport($id);
+		$this->session->set_flashdata('flash', '<div class="alert alert-success" role="alert"> Delete Success</div>');
+		redirect('report');
+	}
+
+	public function submit_update($id){
+
+		if(isset($_FILES['capture'])) {
+			// $data['capture'] = $upload[;file]
+			// $file_name = $strin_filename;
+
+			$config['upload_path'] = './uploads/'; 
+			$config['allowed_types'] = 'jpg|jpeg|png|pdf|'; 
+			$config['max_size'] = '10000'; // max_size in kb 
+			$config['encrypt_name'] = TRUE;
+
+			$this->load->library('upload', $config);
+
+			if ($this->upload->do_upload('capture')) {
+				$capture = $this->upload->data();
+				$capture_name = $capture['file_name'];
+				$data['capture'] = $upload ['file'];
+				$capture_name = $string_capture;
+			}
+		}
+
+		if(isset($_FILES['query'])) {
+				// $data['capture'] = $upload[;file]
+				// $file_name = $strin_filename;
+	
+				$config['upload_path'] = './uploads/'; 
+				$config['allowed_types'] = 'pdf|sql|doc|docx|xlsx'; 
+				$config['max_size'] = '10000'; // max_size in kb 
+				$config['encrypt_name'] = TRUE;
+	
+				$this->load->library('upload', $config);
+	
+				if ($this->upload->do_upload('capture')) {
+					$capture = $this->upload->data();
+					$capture_name = $capture['file_name'];
+					$data['query'] = $upload ['file'];
+					$query_name = $string_query;
+				
+				}
+		}
+
+		$data['title'] = 'Form Update Report';
+		$data['user'] = $this->db->get_where('user', ['email' =>
+						$this->session->userdata('email')])->row_array();
+
+		$this->form_validation->set_rules('nama_report', 'nama_report', 'required');
+
+		if( $this->form_validation->run() == FALSE ){
+			$this->load->view('layouts/header', $data);
+			$this->load->view('layouts/sidebar', $data);
+			$this->load->view('layouts/topbar', $data);
+			$this->load->view('report/update', $data);
+			$this->load->view('layouts/footer', $data);
+		}else{
+			$data = [
+				"nama_report"=>$this->input->post('nama_report', true),
+				"npp"=>$this->input->post('npp', true),
+				"nama"=>$this->input->post('nama', true),
+				"unit"=>$this->input->post('unit', true),
+				"via"=>$this->input->post('via', true),
+				"nomor_email"=>$this->input->post('nomor_email', true),
+				"tgl_permintaan"=>$this->input->post('tgl_permintaan', true),
+				// "capture"=>$capture_name,
+				"pic"=>$this->input->post('pic', true),
+				// "query"=>$query_name,
+				"tgl_diberikan"=>$this->input->post('tgl_diberikan', true),
+				"status"=>$this->input->post('status', true)
+			];
+		
+		$this->Report_model->updateReport($data);
+		$this->session->set_flashdata('flash', '<div class="alert alert-success" role="alert"> Update Success</div>');
+		redirect('report');
+		}
+	}
+
+	
+	public function update($id)
+	{
+		$data['title'] = 'Form Update Report';
+		$data['user'] = $this->db->get_where('user', ['email' =>
+		$this->session->userdata('email')])->row_array();
+		$data['report'] = $this->Report_model->getbyId($id);
+		$data['via'] = ['WhatsApp','Email','Notin','Memo','On The Sport'];
+		$data['status'] = ['On Progress','Done','Cancel'];
+		$data['id'] = $id;
+
+		$this->load->view('layouts/header', $data);
+		$this->load->view('layouts/sidebar', $data);
+		$this->load->view('layouts/topbar', $data);
+		$this->load->view('report/update', $data);
+		$this->load->view('layouts/footer', $data);
+
+		// if( $this->form_validation->run() == false ){
+		// 	$this->load->view('layouts/header', $data);
+		// 	$this->load->view('layouts/sidebar', $data);
+		// 	$this->load->view('layouts/topbar', $data);
+		// 	$this->load->view('report/update', $data);
+		// 	$this->load->view('layouts/footer', $data);
+		// }else{
+		// 	$this->Report_model->updateReport();
+		// 	$this->session->set_flashdata('flash', '<div class="alert alert-success" role="alert"> Update Success</div>');
+		// 	redirect('report');
+		// }
+
+	}
+
 }
---
+++ application/models/Report_model.php
@@ -0,0 +1,44 @@
+<?php 
+
+class Report_model extends CI_Model {
+
+	private $table = 'report';
+
+	public function getAll()
+    {
+        $this->db->from($this->table);
+        $this->db->order_by("id", "asc");
+        $query = $this->db->get();
+        return $query->result();
+        //fungsi diatas seperti halnya query 
+        //select * from mahasiswa order by IdMhsw desc
+    }
+
+	public function insertReport($data)
+	{
+		$this->db->insert('report', $data);
+	}
+
+	public function deleteReport($id)
+	{
+		return $this->db->delete($this->table, ['id' => $id]);
+	}
+
+	public function updateReport($data)
+	{
+		
+		$this->db->where('id', $this->input->post('id'));
+		$this->db->update('report', $data);
+
+	}
+
+
+	public function getById($id)
+    {
+        return $this->db->get_where($this->table, ['id' => $id])->row_array();
+    }
+
+}
+
+
+?>
